04b6bcebb2fe82b8d6acbf6c76dafdec950f2842
Mauro Teixeira <mevyness@gmail.com>
2022-12-11 21:03:41 -0300

    Olha eu de novo

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,28 @@
 <head>
-        <style> @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'); </style>
-        <style> @import url('https://fonts.googleapis.com/css2?family=Anek+Devanagari:wght@600&display=swap'); </style>
-        <link rel="stylesheet" href="./html/index.css"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>The Cult</title>
-        <meta charset="utf-8">
-        <link rel="icon" href="./gif/fogo.gif">
-        <script src="./script.js"></script>
-    </head>
-        <div>
-        <audio id="menuAudio" autoplay loop>
-            <source src="./html/sound/menu sound2.wav" type="audio/wav">
-            <source src="./html/sound/menu sound2.ogg" type="audio/ogg">
-        </audio>
-        </div>
-        <div class="content">
-            <h1 id="title">The Cult</h1>
-            <div class="buttons">
-                <button class="menu"><a href="./html/new game.html">Continuar</a></button>
-                <button class="menu"><a href="./html/new game.html">Novo Jogo</a></button>
-                <button class="menu"><a href="./html/options.html">Opções</a></button>
-                <button class="menu"><a href="./html/how play.html">Como Jogar</a></button>
-                <button class="menu"><a href="./html/about.html">Créditos</a></button>
-            </div>
-        </div>
+    <title>The Cult</title>
+    <style> @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'); </style>
+    <style> @import url('https://fonts.googleapis.com/css2?family=Anek+Devanagari:wght@600&display=swap'); </style>
+    <link rel="stylesheet" href="./html/index.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="icon" href="./gif/fogo.gif">
+    <script src="./script.js"></script>
+</head>
+
+<div>
+    <audio id="menuAudio" autoplay loop>
+        <source src="./html/sound/menu sound2.wav" type="audio/wav">
+        <source src="./html/sound/menu sound2.ogg" type="audio/ogg">
+</audio>
+</div>
+
+    <div class="content">
+        <h1 id="title">The Cult</h1>
+        <div class="buttons">
+            <button class="menu"><a href="./html/new game.html">Continuar</a></button>
+            <button class="menu"><a href="./html/new game.html">Novo Jogo</a></button>
+            <button class="menu"><a href="./html/options.html">Opções</a></button>
+            <button class="menu"><a href="./html/how play.html">Como Jogar</a></button>
+            <button class="menu"><a href="./html/about.html">Créditos</a></button>
+    </div>
+</div>

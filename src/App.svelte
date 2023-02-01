ba9f180d65fac2a1d20ecf0695a2b869ac176f20
Mauro Teixeira <mevyness@gmail.com>
2023-02-01 11:44:33 -0300

    01/02

--- src/App.svelte
+++ src/App.svelte
@@ -11,18 +11,17 @@
 
 <div>
     <audio id="menuAudio" autoplay loop>
-        <source src="./html/sound/menu sound2.wav" type="audio/wav">
-        <source src="./html/sound/menu sound2.ogg" type="audio/ogg">
+        <source src="./html/sound/menu sound.wav" type="audio/wav">
+        <source src="./html/sound/menu sound.ogg" type="audio/ogg">
 </audio>
 </div>
 
     <div class="content">
         <h1 id="title">The Cult</h1>
         <div class="buttons">
-            <button class="menu"><a href="./html/new game.html">Continuar</a></button>
-            <button class="menu"><a href="./html/new game.html">Novo Jogo</a></button>
-            <button class="menu"><a href="./html/options.html">Opções</a></button>
-            <button class="menu"><a href="./html/how play.html">Como Jogar</a></button>
-            <button class="menu"><a href="./html/about.html">Créditos</a></button>
+            <button class="menu"><a href="/map/Map.html">Jogar</a></button>
+            <button class="menu"><a href="/html/options.html">Opções</a></button>
+            <button class="menu"><a href="/html/how play.html">Como Jogar</a></button>
+            <button class="menu"><a href="/html/about.html">Créditos</a></button>
     </div>
 </div>

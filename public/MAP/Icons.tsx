ca92477fd386225255807bc7beff5583015a7d30
Mauro <mevyness@gmail.com>
2023-02-03 19:07:46 -0300

    03/02

---
+++ public/MAP/Icons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Icons" tilewidth="32" tileheight="32" tilecount="432" columns="16">
+ <image source="Sprits/#1 - Transparent Icons.png" width="512" height="867"/>
+</tileset>
---
+++ public/MAP/Props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Props" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Sprits/TX Props.png" width="512" height="512"/>
+</tileset>
